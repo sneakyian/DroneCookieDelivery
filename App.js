ae45c7751744b9a94e4e6c4ada1418243edd76f9
sneakyian <ianmccann194@gmail.com>
2025-07-06 13:27:28 -0500

    This is for Added App and UI pages

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MONGO_URI } from '@env';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+// then use MONGO_URI in your data-access layer
+
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <Text style={styles.text}>Welcome to Drone-Cookie-Delivery!</Text>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 20 }
+});
+
+export default App;
---
+++ metro.config.js
@@ -0,0 +1,9 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * Extends the default config from '@react-native/metro-config'.
+ */
+const { getDefaultConfig } = require('@react-native/metro-config');
+
+module.exports = getDefaultConfig(__dirname);
